3473e843ef5f8a2c9b78396903219241cb54356d
Julian Currie <julian.currie@gmail.com>
2014-02-24 21:27:55 -0800

    Small format changes - old habits die hard

--- src/com/protege/fizzbuzz/WhatsNewDialog.java
+++ src/com/protege/fizzbuzz/WhatsNewDialog.java
@@ -6,15 +6,18 @@ import android.support.v4.app.DialogFragment;
 import android.view.Window;
 import android.widget.TextView;
 
-public class WhatsNewDialog extends DialogFragment {
+public class WhatsNewDialog extends DialogFragment 
+{
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 	}
 
 	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {		
+	public Dialog onCreateDialog(Bundle savedInstanceState) 
+	{		
 		final Dialog newDialog = new Dialog(getActivity());				
 		newDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		newDialog.getWindow().setWindowAnimations(android.R.anim.fade_in);
@@ -24,11 +27,12 @@ public class WhatsNewDialog extends DialogFragment {
 		TextView newText = (TextView)newDialog.findViewById(R.id.whatsnew_text);
 
 		StringBuilder sBuilder = new StringBuilder();
-		
+
 		String[] newArray = getResources().getStringArray(R.array.whats_new);
-		
-		for(String line : newArray)
+
+		for(String line : newArray){
 			sBuilder.append("- " + line + System.getProperty("line.separator") + System.getProperty("line.separator"));
+		}
 
 		newText.setText(sBuilder.toString());
 
